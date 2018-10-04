476369331b4c0fb57461705d1b53b3b571ffaa58
Leandro Cunha <leandroscunha@gmail.com>
2018-10-04 16:51:35 -0300

    feature: apply watermark

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
+import watermarkjs from 'watermarkjs';
 
-const App = () => <h1>#EBMNAO</h1>;
+class App extends Component {
+  componentDidMount() {
+    const file = '../images/user.jpeg';
+    const watermark = '../images/crf.png';
+
+    watermarkjs([file, watermark])
+      .image(watermarkjs.image.lowerRight())
+      .then((img) => {
+        document.getElementById('composite-image').appendChild(img);
+      });
+  }
+
+  render() {
+    return (
+      <section>
+        <h1>#EBMNAO</h1>
+        <div id="composite-image" />
+      </section>
+    );
+  }
+}
 
 export default App;
