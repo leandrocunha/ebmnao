e39fae96088f089de7da3e698360e53cedf1a1a9
Leandro Cunha <leandroscunha@gmail.com>
2018-10-05 13:41:19 -0300

    improvement(layout): sass files and new ribbon

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Footer = () => (
+  <div className="Footer">
+    <div>
+      <p>
+        Este projeto não tenho nenhum cunho político, não é apoiado por nenhuma chapa seja ela de
+        oposição ou situação e nem mesmo pelo Clube de Regatas do Flamengo. A maior razão foi de
+        participar do #hacktoberfest aproveitando o mês eleitoral e as eleições do Clube de Regatas
+        do Flamengo que se aproxima.
+      </p>
+      <p>
+        Um projeto independente, de torcedor para para torcedor, de programador para programador.
+      </p>
+      <p>#SRN</p>
+    </div>
+  </div>
+);
+
+export default Footer;
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 const Header = () => (
   <header className="Header">
-    <h1 className="Header__Brand">#EBMNAO</h1>
+    <h1 className="Header__Brand">
+      <span className="Header__Brand--yellow">#</span>
+      <span className="Header__Brand">EBM</span>
+      <span className="Header__Brand--red">NAO</span>
+    </h1>
+    <img className="Header__CRF" src="./../images/crf.jpg" />
   </header>
 );
 
